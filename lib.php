61432a48d3d412b2cb91a9147a5d182cd802eae3
awaisOptimistic <awais@of.edu.au>
2025-01-23 12:01:31 +1100

    Update lib.php

--- lib.php
+++ lib.php
@@ -306,9 +306,7 @@ function notificationBar(){
                             </a>
                             <!-- Dropdown - Alerts -->
                             <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
-                                <h6 class="dropdown-header">
-                                    Alerts Center
-                                </h6>
+                                
                                 <a class="dropdown-item d-flex align-items-center" href="#">
                                     <div class="mr-3">
                                         <div class="icon-circle bg-success">
@@ -316,8 +314,7 @@ function notificationBar(){
                                         </div>
                                     </div>
                                     <div>
-                                        <div class="small text-gray-500">5</div>
-                                        New Users have signed up!
+                                        <a href="/?page=pendingUsers"><?php echo  $_SESSION['newusers'];?> New Users have signed up!</a>
                                     </div>
                                 </a>
                             </div>
