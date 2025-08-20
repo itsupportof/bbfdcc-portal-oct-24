66a1cbb1aaf06590b0aa86da52e14aaa50b8938e
awaisOptimistic <awais@of.edu.au>
2025-08-20 10:50:35 +1000

    New

--- index.php
+++ index.php
@@ -90,11 +90,8 @@ if(!isset($_GET['page']) && isset($_SESSION['currentSession'])){
                     
                     <!-- Nav Item - User Information and notification -->
                     <?php 
-                    if($role==1 || $role==2
-                    {notificationBar();}
-                    
-                    $b=userOption();echo $b;
-                    ?>
+                    notificationBar();
+                    $b=userOption();echo $b;?>
 
                 </ul>
 
